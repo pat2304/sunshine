f382207bb9b2f9124a8b1d8a935119c082974871
pat2304 <pat2304@gmail.com>
2022-06-28 10:57:59 +0800

    Add # 220626 《跨領域－重磅對話》…時代前沿對話！ 第57集 《維摩詰經》…全球直播教學視頻 主題：雖行於空－植種德本 #chrome_conn('https://www.youtube.com/watch?v=UMNKSNIAt8I', 10, 500) # chrome_conn('https://s.eqxiu.cn/s/SOcpPeOv', 20, 10)
    # 220627 解套．全方位貧窮…第4集《煩惱．DUCK 不必》…#北大聖玄#覺曦軒
    #chrome_conn('https://www.youtube.com/watch?v=B6yBfgAbsHU', 10, 195)
    chrome_conn('https://s.eqxiu.cn/s/cuGXZIcC?bt=yxy', 20, 10)
    
    # 220628 《異次元—解密》信息場奧秘…進入信息場…有方法！ 主題：適者生存－信息場成長與演化
    #chrome_conn('https://v.eqxiu.cn/s/0RWASSU5', 20, 10)

--- LearnSelenium/FirstAutomationTest.py
+++ LearnSelenium/FirstAutomationTest.py
@@ -34,15 +34,26 @@ def chrome_conn (URL, iter, timer):
 # 《異次元—解密》 主題：揭穿－三維空間假相
 #chrome_conn('https://s.eqxiu.cn/s/0lkTXVid', 1, 10)
 
+# 220626 《跨領域－重磅對話》…時代前沿對話！ 第57集 《維摩詰經》…全球直播教學視頻 主題：雖行於空－植種德本
+#chrome_conn('https://www.youtube.com/watch?v=UMNKSNIAt8I', 10, 500)
+# chrome_conn('https://s.eqxiu.cn/s/SOcpPeOv', 20, 10)
+
+# 《異次元—解密》信息場奧秘…進入信息場…有方法！ 主題：適者生存－信息場成長與演化
+#chrome_conn('https://v.eqxiu.cn/s/0RWASSU5', 20, 10)
+
+# 220627 解套．全方位貧窮…第4集《煩惱．DUCK 不必》…#北大聖玄#覺曦軒
+#chrome_conn('https://www.youtube.com/watch?v=B6yBfgAbsHU', 10, 195)
+chrome_conn('https://s.eqxiu.cn/s/cuGXZIcC?bt=yxy', 20, 10)
+
 # 220618 外現聲聞身－內秘菩薩行…第56集《維摩詰經》…#北大聖玄 #覺曦軒
-#chrome_conn('https://www.youtube.com/watch?v=oonruVoXOf0', 1, 480)
+#chrome_conn('https://www.youtube.com/watch?v=oonruVoXOf0', 20, 480)
 
 # 220617 《跨领域－重磅对话》…时代前沿对话！ 第56集 《维摩诘经》…全球直播教学视频 主题：外现声闻身－内秘菩萨行
 #chrome_conn('https://v.eqxiu.cn/s/DBGJLWnW', 10, 10)
 
 # 220625 - 《10分鐘·奇葩＆閨蜜》 臨終關懷起源—人性需求
-chrome_conn('https://www.youtube.com/watch?v=CjCG4lx8DD8', 10, 290)
-#chrome_conn('https://v.eqxiu.cn/s/4gOU847g?bt=yxy', 10, 10)
+# chrome_conn('https://www.youtube.com/watch?v=CjCG4lx8DD8', 10, 290)
+# chrome_conn('https://v.eqxiu.cn/s/4gOU847g?bt=yxy', 10, 10)
 
 # 《聖玄語露·第14-6集》生命中最重要的兩天： 出生當天，以及覺悟生命存在意義的那一天。
 # chrome_conn('https://www.youtube.com/watch?v=6qs9wqdmsSM', 10, 10)
