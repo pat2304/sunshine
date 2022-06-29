693ef21fbfe7b5c4c0bce5f8cd83b65c435580e3
pat2304 <pat2304@gmail.com>
2022-06-29 10:16:02 +0800

    Add 1. try ... except to handle unexpected error 2. # 220629 第63集《10分鐘·奇葩＆閨蜜》 🌺🌺主題：臨終關懷－五全照顧 (善生、善終…生死兩無憾！) # https://www.youtube.com/watch?v=i5vx63X9Ors chrome_conn('https://a.scene.ryxiut.net/s/tH1pXUEb/1656457671824', 50, 10)

--- LearnSelenium/FirstAutomationTest.py
+++ LearnSelenium/FirstAutomationTest.py
@@ -13,7 +13,8 @@ import time
 # driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()))
 # driver = webdriver.Chrome('E:/TEMP/selenium/browser_drivers/chromedriver.exe')
 
-def chrome_conn (URL, iter, timer):
+def chrome_conn(url, iter, timer):
+    error = 0
 
     for i in range(0, iter, 1):
         servs = Service(ChromeDriverManager().install())
@@ -23,7 +24,11 @@ def chrome_conn (URL, iter, timer):
         #driver = webdriver.Chrome(service=servs)
 
         # chrome connect to URL
-        driver.get(URL)
+        try:
+            driver.get(url)
+        except:
+            error += 1
+            print('Error => ', error)
 
         # driver.maximize_window()
         print(driver.title, ' - ', i + 1)
@@ -32,24 +37,32 @@ def chrome_conn (URL, iter, timer):
 
 
 # 《異次元—解密》 主題：揭穿－三維空間假相
-#chrome_conn('https://s.eqxiu.cn/s/0lkTXVid', 1, 10)
+# chrome_conn('https://s.eqxiu.cn/s/0lkTXVid', 1, 10)
+#chrome_conn('https://a.scene.ryxiut.net/s/0lkTXVid/1656467885377', 50, 10)
+
+# 均衡身心靈‧妙法出奇制勝 第6集《10分鐘·奇葩&閨蜜》  主題：出奇制勝－世間+ 出世間法
+#chrome_conn('https://c.scene.ryxiut.net/s/9mUa5tLm/', 10, 10)
 
 # 220626 《跨領域－重磅對話》…時代前沿對話！ 第57集 《維摩詰經》…全球直播教學視頻 主題：雖行於空－植種德本
 #chrome_conn('https://www.youtube.com/watch?v=UMNKSNIAt8I', 10, 500)
 # chrome_conn('https://s.eqxiu.cn/s/SOcpPeOv', 20, 10)
 
-# 《異次元—解密》信息場奧秘…進入信息場…有方法！ 主題：適者生存－信息場成長與演化
-#chrome_conn('https://v.eqxiu.cn/s/0RWASSU5', 20, 10)
+# 220629 第63集《10分鐘·奇葩＆閨蜜》 🌺🌺主題：臨終關懷－五全照顧 (善生、善終…生死兩無憾！)
+# https://www.youtube.com/watch?v=i5vx63X9Ors
+chrome_conn('https://a.scene.ryxiut.net/s/tH1pXUEb/1656457671824', 50, 10)
+
+# 220628 《異次元—解密》信息場奧秘…進入信息場…有方法！ 主題：適者生存－信息場成長與演化
+# chrome_conn('https://v.eqxiu.cn/s/0RWASSU5', 50, 10)
 
 # 220627 解套．全方位貧窮…第4集《煩惱．DUCK 不必》…#北大聖玄#覺曦軒
-#chrome_conn('https://www.youtube.com/watch?v=B6yBfgAbsHU', 10, 195)
-chrome_conn('https://s.eqxiu.cn/s/cuGXZIcC?bt=yxy', 20, 10)
+# chrome_conn('https://www.youtube.com/watch?v=B6yBfgAbsHU', 10, 195)
+# chrome_conn('https://s.eqxiu.cn/s/cuGXZIcC?bt=yxy', 20, 10)
 
 # 220618 外現聲聞身－內秘菩薩行…第56集《維摩詰經》…#北大聖玄 #覺曦軒
-#chrome_conn('https://www.youtube.com/watch?v=oonruVoXOf0', 20, 480)
+# chrome_conn('https://www.youtube.com/watch?v=oonruVoXOf0', 20, 480)
 
 # 220617 《跨领域－重磅对话》…时代前沿对话！ 第56集 《维摩诘经》…全球直播教学视频 主题：外现声闻身－内秘菩萨行
-#chrome_conn('https://v.eqxiu.cn/s/DBGJLWnW', 10, 10)
+# chrome_conn('https://v.eqxiu.cn/s/DBGJLWnW', 10, 10)
 
 # 220625 - 《10分鐘·奇葩＆閨蜜》 臨終關懷起源—人性需求
 # chrome_conn('https://www.youtube.com/watch?v=CjCG4lx8DD8', 10, 290)
@@ -60,13 +73,10 @@ chrome_conn('https://s.eqxiu.cn/s/cuGXZIcC?bt=yxy', 20, 10)
 # chrome_conn'https://v.eqxiu.cn/s/FlHSB3TW?bt=yxy'. 10. 10)
 
 # 共生·共贏－全球戰疫
-#chrome_conn('https://v.eqxiu.cn/s/3zW8XTxG', 10, 10)
+# chrome_conn('https://v.eqxiu.cn/s/3zW8XTxG', 10, 10)
 
 # Set URL 壯闊中的溫柔—獻給人父
 # chrome_conn('https://s.eqxiu.cn/s/QEGRw46u', 10, 10)
-
-# 均衡身心靈‧妙法出奇制勝 第6集《10分鐘·奇葩&閨蜜》  主題：出奇制勝－世間+ 出世間法
-# chrome_conn('https://c.scene.ryxiut.net/s/9mUa5tLm/', 10, 10)
 
 
 # 里外不是人－生死一如
