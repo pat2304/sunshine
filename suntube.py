3e36fb5f0db036d694bdca3a2b64b51958187dce
pat2304 <patrick@micareo.com>
2022-07-18 17:49:42 +0800

    Add files via upload

---
+++ suntube.py
@@ -0,0 +1,32 @@
+from SunFunctions.browser_open import chrome_open
+from SunFunctions.browser_open import fox_open
+from SunFunctions.browser_open import edge_open
+
+# 220622 《10分鐘·奇葩＆閨蜜》第61集; 預立遺囑－臨終關懷; 預立遺囑…時代前沿脈動…跟上得利！
+# chrome_open('https://b.scene.ryxiut.net/s/zCSoSuuy/1657409891884?bt=yxy', 50, 8, 3)
+# chrome_open('https://www.youtube.com/watch?v=mW8HAcnSSkc', 10, 340)
+# chrome_open('https://www.youtube.com/watch?v=mW8HAcnSSkc', 10, 180, 0)
+
+
+# 220711 《煩惱．DUCK 不必》第6集 解套．臨終恐懼五大事…#北大聖玄#覺曦軒 (5:00)
+#chrome_open('https://www.youtube.com/watch?v=rBpaNnwZZlI', 10, 250)
+
+# 220727 菩提之相－實無有法可知…第60集《維摩詰經》…#北大聖玄 #覺曦軒
+chrome_conn('https://www.youtube.com/watch?v=2yBiVxmsn1A', 10, 500)
+
+
+# 220710 觀不生．不入正位－菩薩…第59集《維摩詰經》…#北大聖玄 #覺曦軒
+#chrome_open('https://www.youtube.com/watch?v=R55B_yQASn4', 10, 500)
+#chrome_open('https://www.youtube.com/watch?v=R55B_yQASn4', 10, 200, 0)
+
+# 220717 《視頻化·微享》🌺🌺主題：耕心有成·正道圓成－觀音自在解脫 (4:48)
+chrome_open('https://www.youtube.com/watch?v=UYwuW040ci8', 10, 240)
+#edge_open('https://www.youtube.com/watch?v=UYwuW040ci8', 10, 240)
+
+# 220716 《5分鐘‧奇葩&閨蜜》冷眼紅塵－東方不敗…第68集 (5:42)
+#chrome_open('https://www.youtube.com/watch?v=ml0JabzkPEc', 10, 300)
+#fox_open('https://www.youtube.com/watch?v=ml0JabzkPEc', 10, 300)
+#edge_open('https://www.youtube.com/watch?v=ml0JabzkPEc', 10, 300)
+
+
+
