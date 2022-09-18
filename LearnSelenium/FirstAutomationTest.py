838f522a5cb7539d18cf50b5a80df92c3c0f5057
pat2304 <pat2304@gmail.com>
2022-09-18 12:18:39 +0800

    Add: 1. 220918 行八正道．樂求佛道－菩薩行…第69集《維摩詰經》…#北大聖玄 #覺曦軒 (1:17:41)

--- LearnSelenium/FirstAutomationTest.py
+++ LearnSelenium/FirstAutomationTest.py
@@ -51,11 +51,14 @@ def chrome_conn(url, iter, timer):
         driver.close()
 
 
-# 220913 行七覺分．分別佛智慧－菩薩行…第68集《維摩詰經》…#北大聖玄 #覺曦軒 (1:25:57)
-chrome_conn('https://www.youtube.com/watch?v=0vZq1_McyuM', 60, 480)
+# 220918 行八正道．樂求佛道－菩薩行…第69集《維摩詰經》…#北大聖玄 #覺曦軒 (1:17:41)
+chrome_conn('https://www.youtube.com/watch?v=iG-uPAH4Z6A', 100, 480)
 
 # 220803 關鍵時刻．出奇制勝…文殊八字真言－簡介…北大．聖玄主講 (10:12)
 # chrome_conn('https://www.youtube.com/watch?v=AdgbapI5l6c', 20, 260)
+
+# 220913 行七覺分．分別佛智慧－菩薩行…第68集《維摩詰經》…#北大聖玄 #覺曦軒 (1:25:57)
+# chrome_conn('https://www.youtube.com/watch?v=0vZq1_McyuM', 60, 480)
 
 # 220917 人生最重要的一課…第80集《5分鐘．奇葩 & 閨蜜》…#北大聖玄 #覺曦軒 (5:49)
 # chrome_conn('https://www.youtube.com/watch?v=aLKvImVhWXA', 30, 173)
