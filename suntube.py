fa976d2123d3f3cdb34ae0bda359bec5ffde70c9
pat2304 <pat2304@gmail.com>
2022-10-05 11:46:42 +0800

    Add: 1. 221005 聖人就在你左右…第82集《5分鐘·奇葩&閨蜜》…#北大聖玄 #覺曦軒 (6:33)

--- suntube.py
+++ suntube.py
@@ -10,6 +10,9 @@ chrome_open('https://www.youtube.com/watch?v=8bos67pGPLY', 100, 400)
 # 220919 關鍵出擊－文殊八字精要…第3集《關鍵出擊》…#北大聖玄#覺曦軒 (10:12)
 # chrome_open('https://www.youtube.com/watch?v=YeWu9gP8zP8', 100, 203)
 
+# 221005 聖人就在你左右…第82集《5分鐘·奇葩&閨蜜》…#北大聖玄 #覺曦軒 (6:33)
+# chrome_open('https://www.youtube.com/watch?v=ygUYq9aiWno', 30, 195)
+
 # 221004 賀重陽－AI時尚老人·智慧之巔…18集《視頻化·微享－节庆篇》…#北大聖玄 #覺曦軒 (5:43)
 # chrome_open('https://www.youtube.com/watch?v=-fews4ag4Ec', 30, 170)
 
