88a8aac45532471b9ab03154f7698c073c8593c0
pat2304 <pat2304@gmail.com>
2022-12-17 19:59:19 +0800

    Add: 1. 221217 有相．無相．共振頻率－新人課…第81集《維摩詰經．弟子品》…#北大聖玄 #覺曦軒 (2:02:29)

--- suntube.py
+++ suntube.py
@@ -7,8 +7,11 @@ from SunFunctions.browser_open import edge_open
 # 220919 關鍵出擊－文殊八字精要…第3集《關鍵出擊》…#北大聖玄#覺曦軒 (10:12)
 # chrome_open('https://www.youtube.com/watch?v=YeWu9gP8zP8', 100, 203)
 
+# 221217 有相．無相．共振頻率－新人課…第81集《維摩詰經．弟子品》…#北大聖玄 #覺曦軒 (2:02:29)
+chrome_open('https://www.youtube.com/watch?v=-LUci2iZdgY', 100, 360)
+
 # 221210 生命奧秘．心靈結構－新人課…第80集《維摩詰經．弟子品》…#北大聖玄 #覺曦軒 (1:10:28)
-chrome_open('https://www.youtube.com/watch?v=25ct3rBei4I', 100, 400)
+# chrome_open('https://www.youtube.com/watch?v=25ct3rBei4I', 100, 400)
 
 # 221216 生死一如－不二中道…12-5集《視頻化·聖玄語露》》…#北大聖玄 #覺曦軒 (4:34)
 # chrome_open('https://www.youtube.com/watch?v=ajFhmZFEAqI', 50, 136)
