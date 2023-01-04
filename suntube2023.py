a4bb2972668b33eb0d2b853e8b486e22024a62f1
pat2304 <pat2304@gmail.com>
2023-01-04 22:40:37 +0800

    Add: 1. 230103 生與死－轉換…13集《視頻化•異次元－解密》…#北大聖玄#覺曦軒 (11:34) 2. 230104 新奢華－全心奢華…第74集《5分鐘·奇葩&閨蜜》…#北大聖玄#覺曦軒 (6:49)

--- suntube2023.py
+++ suntube2023.py
@@ -2,12 +2,17 @@ from SunFunctions.browser_open import chrome_open
 from SunFunctions.browser_open import fox_open
 from SunFunctions.browser_open import edge_open
 
+# 230104 新奢華－全心奢華…第74集《5分鐘·奇葩&閨蜜》…#北大聖玄#覺曦軒 (6:49)
+# chrome_open('https://www.youtube.com/watch?v=XuqjFXVATxY', 50, 203)
+
+# 230103 生與死－轉換…13集《視頻化•異次元－解密》…#北大聖玄#覺曦軒 (11:34)
+# chrome_open('https://www.youtube.com/watch?v=iCMIeoe29mQ', 50, 230)
 
 # 230102 母子情深－深似海…第18集《提月點燈－話創異》…#北大聖玄 #覺曦軒 (14:46)
-chrome_open('https://www.youtube.com/watch?v=Tppb1AKj0mg', 50, 220)
+# chrome_open('https://www.youtube.com/watch?v=Tppb1AKj0mg', 50, 220)
 
 # 230101 元旦－普天同慶…34集《視•微享－節慶篇》…#北大聖玄 #覺曦軒 (4:05)
-chrome_open('https://www.youtube.com/watch?v=QKDK08fRk9o', 50, 122)
+# chrome_open('https://www.youtube.com/watch?v=QKDK08fRk9o', 50, 122)
 
 
 
