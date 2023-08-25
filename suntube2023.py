78584e0042c53137c82b7d76ee1f96120ac880ea
pat2304 <pat2304@gmail.com>
2023-08-25 19:21:46 +0800

    Add: 1. 230825 一切施設－勝義善巧…36集…《解深密經》《跨領域重磅對話》…#北京大學聖玄 #覺曦軒 (2:45:55)

--- suntube2023.py
+++ suntube2023.py
@@ -6,11 +6,14 @@ from SunFunctions.browser_open import chrome_open
 # 220919 關鍵出擊－文殊八字精要…第3集《關鍵出擊》…#北大聖玄#覺曦軒 (10:12)
 # chrome_open('https://www.youtube.com/watch?v=YeWu9gP8zP8', 100, 203)
 
+# 230825 一切施設－勝義善巧…36集…《解深密經》《跨領域重磅對話》…#北京大學聖玄 #覺曦軒 (2:45:55)
+chrome_open('https://www.youtube.com/watch?v=fb8LzvlEiGU', 100, 240)
+
 # 230819 諸法不相待－一念不住…108集…《維摩詰經．弟子品》《跨領域－重磅對話》…#北京大學聖玄 #覺曦軒 (1:58:33)
 chrome_open('https://www.youtube.com/watch?v=7hZcve0PBOY', 100, 240)
 
 # 230818 不見意法及諸識－勝義善巧…35集《解深·跨》…《解深密經》《跨領域－重磅對話》…#北京大學聖玄 #覺曦軒 (2:52:38)
-chrome_open('https://www.youtube.com/watch?v=x0dDO8PB1qk', 100, 240)
+# chrome_open('https://www.youtube.com/watch?v=x0dDO8PB1qk', 100, 240)
 
 # 230824 無心 vs. 纏繞－融攝…Transcendence vs. Entanglement－Assimilation…10-9集《視·聖玄語露》 …#北京大學聖玄#覺曦軒 (13:01)
 # chrome_open('https://www.youtube.com/watch?v=oEPaGEM2E04', 50, 194)
