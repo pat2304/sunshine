1bbfea7273c4cbca3902ee48ff55d9c1e5f65b26
pat2304 <pat2304@gmail.com>
2023-09-16 23:30:07 +0800

    Add: 1. 230916 超越假名．離眾過－真出家…112集《維摩·跨領域》…#北京大學聖玄 #覺曦軒 (1:17:02)

--- suntube2023.py
+++ suntube2023.py
@@ -6,11 +6,14 @@ from SunFunctions.browser_open import chrome_open
 # 220919 關鍵出擊－文殊八字精要…第3集《關鍵出擊》…#北大聖玄#覺曦軒 (10:12)
 # chrome_open('https://www.youtube.com/watch?v=YeWu9gP8zP8', 100, 203)
 
+# 230916 超越假名．離眾過－真出家…112集《維摩·跨領域》…#北京大學聖玄 #覺曦軒 (1:17:02)
+chrome_open('https://www.youtube.com/watch?v=l_G6Cpzc9hs', 100, 240)
+
 # 230915 【修正版】一切法－平等真如…39集．教學視頻《解深密經•跨》…#北京大學聖玄 #覺曦軒 (2:37:01)
 chrome_open('https://www.youtube.com/watch?v=APeIksxJrI8', 100, 240)
 
 # 230909 離62見..處於涅槃…111集《維摩詰經．跨》…#北京大學聖玄 #覺曦軒 (1:44:25)
-chrome_open('https://www.youtube.com/watch?v=7eVXXvLCtgE', 100, 240)
+# chrome_open('https://www.youtube.com/watch?v=7eVXXvLCtgE', 100, 240)
 
 # 230914 酷酷雲端女郎—風情萬種…A cool lady monk online－charming and alluring《視·微享》…#北京大學聖玄#覺曦軒 (13:40)
 # chrome_open('https://www.youtube.com/watch?v=OwcIGQshwQ8', 50, 210)
