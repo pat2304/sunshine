2fb2f7f7ada775e46340848a8e1d5d04b84e6bc8
pat2304 <pat2304@gmail.com>
2024-01-05 23:10:53 +0800

    Add: 1. 240101 元旦－普天同慶New Year's Day…83集《視•微享－節慶篇》…#北大聖玄 #覺曦軒 (7:11)

---
+++ suntube2024.py
@@ -0,0 +1,19 @@
+from SunFunctions.browser_open import fox_open
+from SunFunctions.browser_open import edge_open
+from SunFunctions.browser_open import chrome_open
+# import suntube2022
+
+# 220919 關鍵出擊－文殊八字精要…第3集《關鍵出擊》…#北大聖玄#覺曦軒 (10:12)
+# chrome_open('https://www.youtube.com/watch?v=YeWu9gP8zP8', 100, 203)
+
+# 231230 自疾不能救－能救諸疾人…127集《維摩·跨域》…#北京大學聖玄 #覺曦軒 (1:25:39)
+chrome_open('https://www.youtube.com/watch?v=-bV34FV-raA', 100, 240)
+
+# 231229 法無我性－名勝義．無自性性…54集《解深·跨域》…#北京大學聖玄 #覺曦軒》 (1:26:30)
+chrome_open('https://www.youtube.com/watch?v=NxiBm92UkYI', 100, 240)
+
+# 240103 去來自由．心體無滯－般若No Stagnation at all －Prajna…10-12集《視•聖玄語露》…#北京大學聖玄#覺曦軒 (9:19)
+# chrome_open('https://www.youtube.com/watch?v=J_f4HKcesXM', 50, 185)
+
+# 240101 元旦－普天同慶New Year's Day…83集《視•微享－節慶篇》…#北大聖玄 #覺曦軒 (7:11)
+# chrome_open('https://www.youtube.com/watch?v=aY2Jmd345Wo', 50, 215)
