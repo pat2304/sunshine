a9533d6e7bc5d16677822390a7c1d6f31da18318
pat2304 <pat2304@gmail.com>
2024-01-13 06:12:31 +0800

    Add: 1. 240112 雜染不相應－法性安住無為…56集《解深密經．無自性相品》…#北京大學聖玄 #覺曦軒 (1:55:03)

--- suntube2024.py
+++ suntube2024.py
@@ -6,14 +6,17 @@ from SunFunctions.browser_open import chrome_open
 # 220919 關鍵出擊－文殊八字精要…第3集《關鍵出擊》…#北大聖玄#覺曦軒 (10:12)
 # chrome_open('https://www.youtube.com/watch?v=YeWu9gP8zP8', 100, 203)
 
+# 240112 雜染不相應－法性安住無為…56集《解深密經．無自性相品》…#北京大學聖玄 #覺曦軒 (1:55:03)
+chrome_open('https://www.youtube.com/watch?v=5gOmRrxQ_ek', 100, 240)
+
 # 240106 總結凡聖－如理．方便…128集《維摩·跨域》…#北京大學聖玄 #覺曦軒 (1:41:46)
 chrome_open('https://www.youtube.com/watch?v=Ykaevbhh9EE', 100, 240)
 
 # 240105 法無我性－遍一切故…55集《解深·跨域》…#北京大學聖玄 #覺曦軒 (1:51:27)
-chrome_open('https://www.youtube.com/watch?v=MSBS9OZEbdE', 100, 240)
+# chrome_open('https://www.youtube.com/watch?v=MSBS9OZEbdE', 100, 240)
 
 # 240110 般若三昧－無念．Prajna Samadhi... 9-1集《視·聖玄語露》…#北京大學聖玄#覺曦軒 (7:06)
-chrome_open('https://www.youtube.com/watch?v=-C8gZKah4Xc', 50, 213)
+# chrome_open('https://www.youtube.com/watch?v=-C8gZKah4Xc', 50, 213)
 
 # 231230 自疾不能救－能救諸疾人…127集《維摩·跨域》…#北京大學聖玄 #覺曦軒 (1:25:39)
 # chrome_open('https://www.youtube.com/watch?v=-bV34FV-raA', 100, 240)
