67d5e10439ad0efb06b5e56b900197e8af26d8fe
pat2304 <pat2304@gmail.com>
2024-01-27 23:16:57 +0800

    Add: 1. 240127 心．不在內外中－罪．法亦然…131集《維摩•跨域》…#北京大學聖玄 #覺曦軒 (1:05:40)

--- suntube2024.py
+++ suntube2024.py
@@ -6,11 +6,14 @@ from SunFunctions.browser_open import chrome_open
 # 220919 關鍵出擊－文殊八字精要…第3集《關鍵出擊》…#北大聖玄#覺曦軒 (10:12)
 # chrome_open('https://www.youtube.com/watch?v=YeWu9gP8zP8', 100, 203)
 
+# 240127 心．不在內外中－罪．法亦然…131集《維摩•跨域》…#北京大學聖玄 #覺曦軒 (1:05:40)
+chrome_open('https://www.youtube.com/watch?v=AssvGxxch6w', 100, 240)
+
 # 240126 隨起言說－隨覺、隨眠．如如執著…58集…《解深·跨域》…#北京大學聖玄 #覺曦軒 (1:49:50)
 chrome_open('https://www.youtube.com/watch?v=aTDMKZUDH1E', 100, 240)
 
 # 240120 諸法亦然－不出於如…130集《維摩詰經》集《維摩•跨域》…#北京大學聖玄 #覺曦軒 (2:10:00)
-chrome_open('https://www.youtube.com/watch?v=ulCuabgjSTU', 100, 240)
+# chrome_open('https://www.youtube.com/watch?v=ulCuabgjSTU', 100, 240)
 
 # 240119 超越三自性－立三無自性性…57集《解深·跨域》…#北京大學聖玄 #覺曦軒 (1:42:30)
 # chrome_open('https://www.youtube.com/watch?v=FX1Bo8NnZAg', 100, 240)
