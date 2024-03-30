01a2d01fecd775f42112e425217e130d0eee20f5
pat2304 <pat2304@gmail.com>
2024-03-31 00:23:14 +0800

    Add: 1. 240330 視頻…2024 3 30速播 為護法城－受持正法 139集《維摩·弟子品·跨域》 (1:15:44)

--- suntube2024.py
+++ suntube2024.py
@@ -6,11 +6,14 @@ from SunFunctions.browser_open import chrome_open
 # 220919 關鍵出擊－文殊八字精要…第3集《關鍵出擊》…#北大聖玄#覺曦軒 (10:12)
 # chrome_open('https://www.youtube.com/watch?v=YeWu9gP8zP8', 100, 203)
 
+# 240330 視頻…2024 3 30速播 為護法城－受持正法 139集《維摩·弟子品·跨域》 (1:15:44)
+chrome_open('https://www.youtube.com/watch?v=kEjedvS1YvM', 100, 240)
+
 # 240329 識所緣－唯識所現．66集《解深·分別瑜伽品·跨域》…#北京大學．聖玄 #覺曦軒 (1:48:20)
 chrome_open('https://www.youtube.com/watch?v=RYk2st052t8', 100, 240)
 
 # 240323 佛出五濁惡世－斯法．度眾…138集《維摩經·弟子品·跨域》…#北京大學．聖玄 #覺曦軒 (1:07:24)
-chrome_open('https://www.youtube.com/watch?v=xnS1G_mxVy0', 100, 240)
+# chrome_open('https://www.youtube.com/watch?v=xnS1G_mxVy0', 100, 240)
 
 # 240322 奢摩他．毘缽舍那－有異與無異．65集…《解深密經．跨域》…#北京大學．聖玄 #覺曦軒 (1:30:16)
 # chrome_open('https://www.youtube.com/watch?v=bXJtJ7AV4Hs', 100, 240)
